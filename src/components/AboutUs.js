55c7b12a0bce3e108b283b249a39e9603beeb277
Chinmayi-2003 <chinmayi.cs21@bmsce.ac.in>
2023-06-11 09:34:54 +0530

    better navbar

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -18,7 +18,7 @@ export default function AboutUs() {
              <div className="row" id="a-row">
                  <div className="col-md-4" id="a-col">
                     <div className="about-item text-center">
-                     <i className="fa fa-book"></i>
+                     <i className="fa fa-book fa-3x"></i>
                      <h3>MISSION</h3>
                      <hr/>
                      <p> The entire economy is being sustained by the field of agriculture. Our mission is to aid in the emerging challenges that we will fight together with the excellent sales & service of agricultural products and implements.  </p>
@@ -26,7 +26,7 @@ export default function AboutUs() {
                  </div>
                  <div className="col-md-4" id="a-col">
                     <div className="about-item text-center">
-                     <i className="fa fa-globe"></i>
+                     <i className="fa fa-globe fa-3x"></i>
                      <h3>VISION</h3>
                      <hr/>
                      <p id="vision">Our vision is to enhance the developing sector of the sales and services of agriculture products. We aim to express, inspire and stand as a role model to all emerging technologies that share this specific domain. </p>
@@ -34,7 +34,7 @@ export default function AboutUs() {
                  </div>
                  <div className="col-md-4" id="a-col">
                     <div className="about-item text-center">
-                     <i className="fa fa-pencil"></i>
+                     <i class="fa fa-pencil fa-3x" ></i>
                      <h3>JOURNEY</h3>
                      <hr/>
                      <p>Saya Enterprises started its journey in 2002. We have succesfully completed the last 20 years with utmost customer satisfaction. We have a very hardworking team of staff always aiming to strive for greatness in the domain of agriculture.</p>
