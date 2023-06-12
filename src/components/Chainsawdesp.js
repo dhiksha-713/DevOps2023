0b7ff59d4d588a4bd4dc5693ceb100552cf575d0
Deeksha <deeksha.cs21@bmsce.ac.in>
2023-06-12 21:50:05 +0530

    Chainsaw edited!

--- src/components/Chainsawdesp.js
+++ src/components/Chainsawdesp.js
@@ -1,93 +1,116 @@
-import React from 'react'
+import React from "react";
 
 export default function Chainsawdesp() {
   return (
     <div>
-        <div className="container">
-            <div className="detail-conatiner" id="prod11">
-                <div className="prodoct_img">
-                    <div className="p_img">
-                        <img src="./images/STIHL MS250 2.png"/>
-                    </div>
-                    <div className="purchase">
-                        <button><a href="cart.php"><i className="fa-solid fa-cart-shopping"></i>Add to cart</a></button>
-                    </div>
-                </div>
-    <div className="prod_detail">
-        <div className="p_name">
-            <h1>STIHL chainsaw</h1>
-            <p className="p_price">₹15,000 </p>
+      <div className="container">
+        <div className="detail-conatiner" id="prod11">
+          <div className="prodoct_img">
+            <div className="p_img">
+              <img src="./images/STIHL MS250 2.png" />
+            </div>
+            <div className="purchase">
+              <button>
+                <a href="cart.php">
+                  <i className="fa-solid fa-cart-shopping"></i>Add to cart
+                </a>
+              </button>
+            </div>
+          </div>
+          <div className="prod_detail">
+            <div className="p_name">
+              <h1>STIHL chainsaw</h1>
+              <p className="p_price">₹15,000 </p>
+            </div>
+            <div className="p_description">
+              <br />
+              <h3>Specifications</h3>
+              <br />
+              Weight: 4.6 Kg
+              <br />
+              Displacement: 45.4 CC <br />
+              Engine Power: 2.3 kw <br />
+              HP: 3.1 HP <br />
+              Type of Product: Petrol Chain Saw <br />
+              Model No: MS 250 <br />
+              Guide Bar Length: 20 Inch <br />
+              Saw Chain Pitch: 3/8" P <br />
+            </div>
+          </div>
         </div>
-        <div className="p_description"> 
-            <br>
-        <h3>Specifications</h3><br>
-        Weight: 4.6 Kg<br>
-        Displacement: 45.4 CC <br>
-        Engine Power: 2.3 kw <br>
-        HP: 3.1 HP <br>
-        Type of Product: Petrol Chain Saw <br>
-        Model No: MS 250 <br>
-        Guide Bar Length: 20 Inch <br>
-        Saw Chain Pitch: 3/8" P <br>
-        </div>    
-    </div>   
-</div>
-<div className="detail-conatiner" id="prod12">
-    <div className="prodoct_img">
-        <div className="p_img">
-            <img src="./images/OM GS35 shadow.png">
+        <div className="detail-conatiner" id="prod12">
+          <div className="prodoct_img">
+            <div className="p_img">
+              <img src="./images/OM GS35 shadow.png" />
+            </div>
+            <div className="purchase">
+              <button>
+                <a href="cart.php">
+                  <i className="fa-solid fa-cart-shopping"></i>Add to cart
+                </a>
+              </button>
+            </div>
+          </div>
+          <div className="prod_detail">
+            <div className="p_name">
+              <h1>OM GS35 shadow</h1>
+              <p className="p_price">₹15,000 </p>
+            </div>
+            <div className="p_description">
+              <br />
+              <h3>Specifications</h3>
+              <br />
+              Power/Displacement: 2.0 HP - 1.5 kW/38.9 cm³
+              <br />
+              Bar length (nose): 35-41 cm, 14''-16'', (sprocket)
+              <br />
+              Chain (pitch x gauge): 3/8''x.050''
+              <br />
+              Oil Pump: automatic, zero flow rate at idle speed
+              <br />
+              Oil/fuel tank capacity: 0.26 L/0.36 L<br />
+              Sound pressure/power level: 99.6/110.4 dB(A)
+              <br />
+              Lh/rh vibration level: 6.0/6.7 m/s²
+              <br />
+              Dry weight without bar and chain: 4.6 kg
+              <br />
+            </div>
+          </div>
         </div>
-        <div className="purchase">
-            <button><a href="cart.php"><i className="fa-solid fa-cart-shopping"></i>Add to cart</a></button>
+        <div className="detail-conatiner" id="prod13">
+          <div className="prodoct_img">
+            <div className="p_img">
+              <img src="./images/jag CS.png" />
+            </div>
+            <div className="purchase">
+              <button>
+                <a href="cart.php">
+                  <i className="fa-solid fa-cart-shopping"></i>Add to cart
+                </a>
+              </button>
+            </div>
+          </div>
+          <div className="prod_detail">
+            <div className="p_name">
+              <h1>Jag CS</h1>
+              <p className="p_price">₹15,000 </p>
+            </div>
+            <div className="p_description">
+              <br />
+              <h3>Specifications</h3>
+              <br />
+              Displacement: 52 CC <br />
+              Rated Power: 2.2kw/8000rpm
+              <br />
+              Fuel Tank Capacity: 550ml
+              <br />
+              Oil Tank Capacity: 260ml <br />
+              Idle Speed: 3000rpm <br />
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-    <div className="prod_detail">
-        <div className="p_name">
-            <h1>OM GS35 shadow</h1>
-            <p className="p_price">₹15,000 </p>
-        </div>
-        <div className="p_description"> 
-            <br>
-        <h3>Specifications</h3><br>
-        Power/Displacement: 2.0 HP - 1.5 kW/38.9 cm³<br>
-        Bar length (nose): 35-41 cm, 14''-16'', (sprocket)<br>
-        Chain (pitch x gauge): 3/8''x.050''<br>
-        Oil Pump: automatic, zero flow rate at idle speed<br>
-        Oil/fuel tank capacity: 0.26 L/0.36 L<br>
-        Sound pressure/power level: 99.6/110.4 dB(A)<br>
-        Lh/rh vibration level: 6.0/6.7 m/s²<br>
-        Dry weight without bar and chain: 4.6 kg<br>
-
-    </div>   
-</div>
-</div>
-<div className="detail-conatiner" id="prod13">
-    <div className="prodoct_img">
-        <div className="p_img">
-            <img src="./images/jag CS.png">
-        </div>
-        <div className="purchase">
-            <button><a href="cart.php"><i className="fa-solid fa-cart-shopping"></i>Add to cart</a></button>
-        </div>
-    </div>
-    <div className="prod_detail">
-        <div className="p_name">
-            <h1>Jag CS</h1>
-            <p className="p_price">₹15,000 </p>
-        </div>
-        <div className="p_description"> 
-            <br>
-        <h3>Specifications</h3><br>
-        Displacement: 52 CC <br>
-        Rated Power: 2.2kw/8000rpm<br>
-        Fuel Tank Capacity: 550ml<br>
-        Oil Tank Capacity: 260ml <br>
-        Idle Speed: 3000rpm <br>
-        </div>  
-    </div>  
-</div>
-
-</div>
-</div>
-  )
+  );
 }
