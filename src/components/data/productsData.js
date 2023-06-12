317503e9b6301718932c2d36c6b3c34eba714419
Chinmayi-2003 <chinmayi.cs21@bmsce.ac.in>
2023-06-12 20:41:23 +0530

    new products page

---
+++ src/components/data/productsData.js
@@ -0,0 +1,60 @@
+const product_category = [
+    {
+        id: 1,
+        product_name: "Chainsaw",
+        description: "Description",
+        thumb: "./images/chainsaw.png"
+    },
+    {
+        id: 2,
+        product_name: "Brushcutter",
+        description: "Description",
+        thumb: "./images/Oleomac.png"
+    },
+    {
+        id: 3,
+        product_name: "Tiller",
+        description: "Description",
+        thumb: "./images/kisan kraft rotary tiller.png"
+    },
+    {
+        id: 4,
+        product_name: "TreeBike",
+        description: "Description",
+        thumb: "./images/Areca Tree bike.png"
+    },
+    {
+        id: 5,
+        product_name: "Tractor",
+        description: "Description",
+        thumb: "./images/Tractors.png"
+    },
+    {
+        id: 6,
+        product_name: "A-Dehuskar",
+        description: "Description",
+        thumb: "./images/areca dehusker 4 belt.png"
+    },
+    
+    {
+        id: 7,
+        product_name: "Motocart",
+        description: "Description",
+        thumb: "./images/Motocart M 350.png"
+    },
+    {
+        id: 8,
+        product_name: "Chaufcutter",
+        description: "Description",
+        thumb: "./images/Chaufcutter1.png"
+    },
+    // {
+    //     id: 9,
+    //     product_name: "Gardening Tools",
+    //     description: "Description",
+    //     thumb: "./images/Gardening Tools.png"
+    // }
+]
+
+export default product_category;
+
