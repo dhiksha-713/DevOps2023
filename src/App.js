ae90a130b1d599f49c9a221c58e752ece5322a05
Chinmayi-2003 <chinmayi.cs21@bmsce.ac.in>
2023-06-15 23:14:35 +0530

    login css

--- src/App.js
+++ src/App.js
@@ -1,39 +1,4 @@
-// import './App.css';
-// import Home from './components/Home';
-// import About from './components/AboutUs';
-// import Products from './components/Products';
-// import Branches from './components/Branches';
-// import Gallery from './components/Gallery';
-// import Developers from './components/Developers'
-// import Navbar from './components/Navbar';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-// import Footer from './components/Footer';
 
-// function App() {
-//   return (
-//     <>
-//       <Router>
-//       <Navbar />
-      
-//         <Routes>
-        
-//           <Route path='/' Component = {Home} exact></Route>
-//           <Route path='/about' Component={About} exact></Route>
-//           <Route path='/products' Component = {Products} exact></Route>
-//           <Route path='/branch' Component = {Branches} exact></Route>
-//           <Route path='/gallery' Component={Gallery} exact></Route>
-//           <Route path='/developers' Component={Developers} exact></Route>
-//         </Routes>
-//       </Router>
-      
-
-//       <Footer/>
-//     </>
-//   );
-// }
-
-
-// export default App;
 import Home from './components/Home';
 import About from './components/AboutUs';
 import { useState } from 'react';
@@ -45,8 +10,6 @@ import Navbar from './components/Navbar';
 import Login from './components/Login';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Footer from './components/Footer';
-import Login from './components/Login';
-
 
 function App() {
   const [flag, setFlag] = useState(true);
