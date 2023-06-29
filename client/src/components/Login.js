7a3e253427c4d4f73609b03fed76637a1f927501
Chinmayi-2003 <chinmayi.cs21@bmsce.ac.in>
2023-06-29 16:08:07 +0530

    database

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Link ,useNavigate} from "react-router-dom"
 import { ToastContainer, toast } from 'react-toastify';
-import "./mix.css"
+import "./style/mix.css"
 
 const Login = () => {
 
