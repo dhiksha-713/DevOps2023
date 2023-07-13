5776e995b32363e5aae9346539f794181085d239
Chinmayi-2003 <chinmayi.cs21@bmsce.ac.in>
2023-07-13 21:58:38 +0530

    product resolve

--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -7,8 +7,6 @@ import { Link, useParams, useNavigate } from 'react-router-dom';
 import { getProductDetails } from '../redux/actions/productActions';
 import { addToCart } from '../redux/actions/cartActions';
 
-import impchainsaw from '../components/images/STIHL MS250 2.png';
-
 export default function ProductScreen() {
   const [qty, setQty] = useState(1);
   const dispatch = useDispatch();
@@ -18,7 +16,7 @@ export default function ProductScreen() {
   const productDetails = useSelector((state) => state.getProductDetails);
   const { loading, error, product } = productDetails;
 
-  console.log(id);
+  // console.log(id);
 
   useEffect(() => {
     if (product && id && id !== product._id) {
@@ -30,42 +28,52 @@ export default function ProductScreen() {
     dispatch(addToCart(product._id, qty));
     navigate('/cart'); // Use the navigate function to navigate to '/cart'
   };
+// console.log(product.imageUrl)
   return (
     <div className='productscreen divbody' >
-        <div className="productscreen__left">
+        {loading ? <h2>Loading...</h2> : error ?<h2>{error}</h2>: (
+            <>
+            <div className="productscreen__left">
             <div className="left__image">
-                <img src={process.env.PUBLIC_URL+impchainsaw} alt=''/>
+                <img src={product.imageUrl} alt={product.name} />
             </div>
-        <div className="left__info">
-            <p className='left__name'>Chainsaw 1</p>
-            <p>Price: ₹1499</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem eveniet maxime reprehenderit et molestias ea nihil eius tempora illum? quia iusto assumenda neque optio libero soluta maxime!</p>
-        </div>
-        </div>  
-        <div className="productscreen__right">
-            <div className='right__info'>
-                <p>
-                    Price: <span>₹14999</span>
-                </p>
-                <p>
-                    Status: <span>In Stock</span>
-                </p>
-                <p>
-                    Qty
-                    <select>
+            <div className="left__info">
+              <p className="left__name">{product.name}</p>
+              <p>Price: ${product.price}</p>
+              <p>Description: {product.description}</p>
+            </div>
+          </div>
+          <div className="productscreen__right">
+            <div className="right__info">
+              <p>
+                Price:
+                <span>${product.price}</span>
+              </p>
+              <p>
+                Status:
+                <span>
+                  In Stock
+                </span>
+              </p>
+              <p>
+                Qty
+                <select value={qty} onChange={(e) => setQty(e.target.value)}>
                         <option value="1">1</option>
                         <option value="2">2</option>
                         <option value="3">3</option>
                         <option value="4">4</option>
                     </select>
-                </p>
-                <p>
-                <Link to="/cart" className='button_cart'>
-                    Add to Cart
-                    </Link>
-                </p>
+              </p>
+              <p>
+                <button className='button_cart' type="button" onClick={addToCartHandler}>
+                  Add To Cart
+                </button>
+              </p>
             </div>
-        </div>
+          </div>
+            </>
+        )}
+        
     </div>
   );
 }
